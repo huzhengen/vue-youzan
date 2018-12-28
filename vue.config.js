a8f3e2fbbe15f9722ebd9a0bcef385e9decd3125
huzhengen <huzhengen@163.com>
2018-12-28 16:20:57 +0800

    Vue CLI3 多页面

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,38 @@
+// vue.config.js
+const path = require('path')
+
 module.exports = {
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/pages/index/index.js',
+            // 模板来源
+            template: 'src/pages/index/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Index Page',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        cart: {
+            // page 的入口
+            entry: 'src/pages/cart/index.js',
+            // 模板来源
+            template: 'src/pages/cart/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'cart.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Cart Page',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'cart']
+        },
+    },
     baseUrl: process.env.NODE_ENV === 'production' ?
-        '/vue-youzan/dist/' :
-        '/'
+        '/vue-youzan/dist/' : '/',
+
 }
