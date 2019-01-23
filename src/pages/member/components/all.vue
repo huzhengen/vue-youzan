929e1d601f5283369d74ed3a4fe6b2491d36473b
卢方 <huzhengen@163.com>
2019-01-23 18:57:15 +0800

    地址管理、地址列表、地址修改、地址新增

--- src/pages/member/components/all.vue
+++ src/pages/member/components/all.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="container" style="min-height: 597px;">
     <div class="block-list address-list section section-first js-no-webview-block">
-      <a
-        class="block-item js-address-item address-item"
-        href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;id=69150287&amp;from="
-      >
+      <a class="block-item js-address-item address-item" @click="toEdit">
         <div class="address-title">tony 13112345678</div>
         <p>广东省珠海市香洲区南方软件园</p>
+        <a class="address-edit">修改</a>
       </a>
       <a
         class="block-item js-address-item address-item address-item-default"
@@ -17,10 +15,20 @@
       </a>
     </div>
     <div class="block stick-bottom-row center">
-      <a
-        class="btn btn-blue js-no-webview-block js-add-address-btn"
-        href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;from="
-      >新增地址</a>
+      <router-link to="form" class="btn btn-blue js-no-webview-block js-add-address-btn">新增地址</router-link>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    toEdit() {
+      this.$router.push({ path: "/address/form" });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
