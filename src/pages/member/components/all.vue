5c8428a773c426b1fc7b7578c930744808e041f6
卢方 <huzhengen@163.com>
2019-01-23 20:45:05 +0800

    渲染地址列表

--- src/pages/member/components/all.vue
+++ src/pages/member/components/all.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="container" style="min-height: 597px;">
-    <div class="block-list address-list section section-first js-no-webview-block">
-      <a class="block-item js-address-item address-item" @click="toEdit">
-        <div class="address-title">tony 13112345678</div>
-        <p>广东省珠海市香洲区南方软件园</p>
+    <div
+      class="block-list address-list section section-first js-no-webview-block"
+      v-if="lists&&lists.length"
+    >
+      <a
+        class="block-item js-address-item address-item"
+        :class="{'address-item-default':list.isDefault}"
+        @click="toEdit"
+        v-for="list in lists"
+        :key="list.id"
+      >
+        <div class="address-title">{{list.name}} {{list.tel}}</div>
+        <p>{{list.provinceName}}{{list.cityName}}{{list.districName}}{{list.address}}</p>
         <a class="address-edit">修改</a>
       </a>
-      <a
-        class="block-item js-address-item address-item address-item-default"
-        href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;id=69150193&amp;from="
-      >
-        <div class="address-title">tony 13112345678</div>
-        <p>北京市北京市东城区天安门</p>
-      </a>
     </div>
+    <div v-if="lists&&!lists.length">还没有地址，请添加</div>
     <div class="block stick-bottom-row center">
       <router-link to="form" class="btn btn-blue js-no-webview-block js-add-address-btn">新增地址</router-link>
     </div>
@@ -21,7 +24,18 @@
 </template>
 
 <script>
+import Address from "js/addressService.js";
 export default {
+  data() {
+    return {
+      lists: null
+    };
+  },
+  created() {
+    Address.list().then(res => {
+      this.lists = res.data.data._req.lists;
+    });
+  },
   methods: {
     toEdit() {
       this.$router.push({ path: "/address/form" });
