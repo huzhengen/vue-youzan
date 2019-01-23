7d71918161096d1e6f18e4e9cab08870ebe4415f
卢方 <huzhengen@163.com>
2019-01-23 18:32:59 +0800

    个人中心页面用vue-router

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,18 @@
-import './member_base.css'
-import './member.css'
+import Vue from 'vue'
+import Router from 'vue-router'
+import member from './components/member.vue'
+Vue.use(Router)
+
+let routes = [{
+    path: '/',
+    component: member
+}]
+
+let router = new Router({
+    routes
+})
+
+new Vue({
+    el: '#app',
+    router
+})
