8adc9c9fec45f9977de83f8399b3219b1d288ac1
卢方 <huzhengen@163.com>
2019-01-25 15:56:57 +0800

    完善细节

--- src/pages/member/components/member.vue
+++ src/pages/member/components/member.vue
@@ -18,8 +18,7 @@
             <a
               class="link clearfix relative link-topay"
               data-for="topay"
-              href="https://h5.youzan.com/v2/trade/record/alllist?type=topay"
-              target="_blank"
+              href="javascript:;"     
             >
               <p class="title-info c-black font-size-12">待付款</p>
             </a>
@@ -28,8 +27,7 @@
             <a
               class="link clearfix relative link-totuan"
               data-for="totuan"
-              href="https://h5.youzan.com/v2/trade/record/alllist?type=totuan"
-              target="_blank"
+              href="javascript:;"
             >
               <p class="title-info c-black font-size-12">待接单</p>
             </a>
@@ -38,8 +36,7 @@
             <a
               class="link clearfix relative link-tosend"
               data-for="tosend"
-              href="https://h5.youzan.com/v2/trade/record/alllist?type=tosend"
-              target="_blank"
+              href="javascript:;"
             >
               <p class="title-info c-black font-size-12">待发货</p>
             </a>
@@ -48,8 +45,7 @@
             <a
               class="link clearfix relative link-send"
               data-for="send"
-              href="https://h5.youzan.com/v2/trade/record/alllist?type=send"
-              target="_blank"
+              href="javascript:;"
             >
               <p class="title-info c-black font-size-12">待收货</p>
             </a>
@@ -57,8 +53,7 @@
           <li>
             <a
               class="link clearfix relative link-rights"
-              href="https://h5.youzan.com/v2/trade/record/rightsOrder"
-              target="_blank"
+              href="javascript:;"
             >
               <p class="title-info c-black font-size-12">退款/维权</p>
             </a>
@@ -67,8 +62,7 @@
         <div class="block block-list list-vertical">
           <a
             class="block-item link clearfix ico-order"
-            href="https://h5.youzan.com/v2/trade/record/alllist?type=all"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">全部订单</p>
           </a>
@@ -76,29 +70,25 @@
         <div class="block block-list list-vertical">
           <a
             class="block-item link clearfix ico-favior js-login"
-            href="https://maijia.youzan.com/mars/collect/goods"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">收藏的商品</p>
           </a>
           <a
             class="block-item link clearfix ico-shop js-login"
-            href="https://maijia.youzan.com/mars/collect/shop"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">收藏的店铺</p>
           </a>
           <a
             class="block-item link clearfix ico-bought js-login"
-            href="https://maijia.youzan.com/mars/record/bought"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">我买过的</p>
           </a>
           <a
             class="block-item link clearfix ico-history js-login"
-            href="https://maijia.youzan.com/mars/record/goodsbrowse"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">我的浏览记录</p>
           </a>
@@ -106,29 +96,25 @@
         <div class="block block-list list-vertical">
           <a
             class="block-item link clearfix ico-card"
-            href="https://maijia.youzan.com/mars/record/MemberCard"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">我的会员卡</p>
           </a>
           <a
             class="block-item link clearfix ico-coupon"
-            href="https://h5.youzan.com/v2/ump/promocard/crosssite?empty_redirect=yzapp"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">我的优惠券</p>
           </a>
           <a
             class="block-item link clearfix ico-luckymoney hide"
-            href="https://h5.youzan.com/v2/trade/record/luckymoney?f_platform=yzapp"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">我的红包</p>
           </a>
           <a
             class="block-item link clearfix ico-backs"
-            href="https://h5.youzan.com/v2/trade/record/backs?f_platform=yzapp"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">我的返现</p>
           </a>
@@ -139,8 +125,7 @@
           </router-link>
           <a
             class="block-item link clearfix ico-help"
-            href="http://bbs.youzan.com/home.php?mod=space&amp;do=thread&amp;view=me&amp;order=lastpost&amp;mobile=2&amp;fid=85&amp;filter=common&amp;in_app_type=weixin"
-            target="_blank"
+            href="javascript:;"
           >
             <p class="title-info c-black font-size-14">联系有赞客服</p>
           </a>
