da1f4eda0a5e1cc593a2bb4935d9cb91661499b4
卢方 <huzhengen@163.com>
2019-02-27 19:22:56 +0800

    完善细节

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
                 plugins: [
 
                     new CompressionPlugin({
-                        test: /\.js$|\.html$|.\css/, //匹配文件名
+                        test: /\.js$|\.html$|\.css/, //匹配文件名
                         threshold: 10240,//对超过10k的数据压缩
                         deleteOriginalAssets: false //不删除源文件
                     })
